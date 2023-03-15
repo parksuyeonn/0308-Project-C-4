71633ddd337b7bc80ab899e279aaa080fcf9eb03
KimEunA97 <kimeuna9712@gmail.com>
2023-03-15 12:07:44 +0900

    랜덤포켓몬 교체 시도

--- mainContainer.js
+++ mainContainer.js
@@ -1,16 +1,19 @@
 const body = document.getElementsByTagName("body");
 const root = document.getElementById('root');
 const mainContainer = document.getElementById('mainContainer');
+
 // 태그 생성 함수
 function CreateTag(tagname) {
   let element = document.createElement(tagname);
   return element;
 }
+
 //태그 선택 함수
 function getTagname(get) {
   let getTag = document.getElementsByTagName(get);
   return getTag;
 }
+
 //div 생성 후 컨테이너 자식으로 넣기
 for (let i = 0; i < 6; i++) {
   let div = CreateTag("div");
@@ -18,8 +21,12 @@ for (let i = 0; i < 6; i++) {
   let img = CreateTag('img');
   div.appendChild(img);
 }
+
+
 mainContainer.style.display = "flex";
 mainContainer.style.flexDirection = "row";
+
+
 function pokemonBoxStyle(w, h, bg, bor) {
   let pokemonBoxLength = mainContainer.children;
   for (let i = 0; i < pokemonBoxLength.length; i++) {
@@ -27,12 +34,12 @@ function pokemonBoxStyle(w, h, bg, bor) {
     let pokemonBox = mainContainer.children[i];
     pokemonBox.style.width = w;
     pokemonBox.style.height = h;
-    pokemonBox.style.backgroundColor = 
+    pokemonBox.style.backgroundColor = bg;
     pokemonBox.style.border = bor;
   }
 }
 //스타일링 값 입력 (width, height, bgColor, border)
-pokemonBoxStyle("200px", "200px", "green", "1px solid black");
+pokemonBoxStyle("200px", "200px", "white", "1px solid black");
 let array = [];
 let newArrLength = 0;
 do {
@@ -53,19 +60,52 @@ const _URL2 = `https://pokeapi.co/api/v2/pokemon/${array[2]}`;
 const _URL3 = `https://pokeapi.co/api/v2/pokemon/${array[3]}`;
 const _URL4 = `https://pokeapi.co/api/v2/pokemon/${array[4]}`;
 const _URL5 = `https://pokeapi.co/api/v2/pokemon/${array[5]}`;
-const URL =[`https://pokeapi.co/api/v2/pokemon/${array[0]}`,`https://pokeapi.co/api/v2/pokemon/${array[1]}`,`https://pokeapi.co/api/v2/pokemon/${array[2]}`,`https://pokeapi.co/api/v2/pokemon/${array[3]}`,`https://pokeapi.co/api/v2/pokemon/${array[4]}`,`https://pokeapi.co/api/v2/pokemon/${array[5]}`]
+const URL = [`https://pokeapi.co/api/v2/pokemon/${array[0]}`, `https://pokeapi.co/api/v2/pokemon/${array[1]}`, `https://pokeapi.co/api/v2/pokemon/${array[2]}`, `https://pokeapi.co/api/v2/pokemon/${array[3]}`, `https://pokeapi.co/api/v2/pokemon/${array[4]}`, `https://pokeapi.co/api/v2/pokemon/${array[5]}`]
 
 
-for(let i =0;i<6;i++){
-window.addEventListener('load',()=>{
-const xhr = new XMLHttpRequest();
-  xhr.open('GET', URL[i]);
-  xhr.send();
+for (let i = 0; i < 6; i++) {
+  window.addEventListener('load', () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', URL[i]);
+    xhr.send();
     xhr.addEventListener('load', function () {
       const _PokeData = JSON.parse(xhr.response);
       mainContainer.children[i].children[0].src = _PokeData.sprites.front_default;
     })
-      
-      //fetch(_URL).then((response)=> response.json()).then((data){img.src=data.sprites.front_default})
-  })  
-  }
+
+    //fetch(_URL).then((response)=> response.json()).then((data){img.src=data.sprites.front_default})
+  })
+}
+
+const firstImgBox = randomContainer.children[0];
+
+mainContainer.children[0].addEventListener('click', function () {
+  let a = Math.floor(Math.random() * 1000 + 1);
+  const _URL = `https://pokeapi.co/api/v2/pokemon/${a}`;
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', _URL);
+  xhr.send();
+  xhr.addEventListener('load', function () {
+    const _PokeData = JSON.parse(xhr.response);
+    img1.src = _PokeData.sprites.front_default;
+
+    randomContainer.addEventListener('mouseover', function () {
+      if (_PokeData.sprites.back_default == null) {
+        img1.src = _PokeData.sprites.front_default
+  
+      } else {
+        img1.src = _PokeData.sprites.back_default
+  
+      }
+    })
+    randomContainer.addEventListener('mouseout', function () {
+      img1.src = _PokeData.sprites.front_default
+  
+    })
+    
+
+
+  })
+
+})
--- randomContainer.js
+++ randomContainer.js
@@ -1,69 +1,69 @@
-const randomContainer=document.getElementById('randomContainer');
+const randomContainer = document.getElementById('randomContainer');
 
-function Maker(tagName,parent,setAttriName,setAttriValue,text){
-  const Element=document.createElement(tagName);
+function Maker(tagName, parent, setAttriName, setAttriValue, text) {
+  const Element = document.createElement(tagName);
   parent.appendChild(Element);
-  Element.setAttribute(setAttriName,setAttriValue);
-  Element.textContent=text;
+  Element.setAttribute(setAttriName, setAttriValue);
+  Element.textContent = text;
 }
 
-let img=Maker('img',randomContainer,'id','img1')
+let img = Maker('img', randomContainer, 'id', 'img1')
 
 let num = Math.floor((Math.random() * 1260)) + 1
 //let num = 953
 const _URL = `https://pokeapi.co/api/v2/pokemon/${num}`;
 
 const xhr = new XMLHttpRequest();
-let beforebutton=Maker('button',randomContainer,'id','before',"←")
-let nextbutton=Maker('button',randomContainer,'id','next',"→")
+let beforebutton = Maker('button', randomContainer, 'id', 'before', "←")
+let nextbutton = Maker('button', randomContainer, 'id', 'next', "→")
 
-randomContainer.children[1].addEventListener('click',function(){
+randomContainer.children[1].addEventListener('click', function () {
   num--;
-const _URL = `https://pokeapi.co/api/v2/pokemon/${num}`;
+  const _URL = `https://pokeapi.co/api/v2/pokemon/${num}`;
   xhr.open('GET', _URL);
   xhr.send();
-  xhr.addEventListener('load',function() {
-  const _PokeData = JSON.parse(xhr.response);
-  img1.src=_PokeData.sprites.front_default;
+  xhr.addEventListener('load', function () {
+    const _PokeData = JSON.parse(xhr.response);
+    img1.src = _PokeData.sprites.front_default;
+  })
 })
-})
-randomContainer.children[2].addEventListener('click',function(){
+randomContainer.children[2].addEventListener('click', function () {
   num++;
-const _URL = `https://pokeapi.co/api/v2/pokemon/${num}`;
+  const _URL = `https://pokeapi.co/api/v2/pokemon/${num}`;
   xhr.open('GET', _URL);
   xhr.send();
-  xhr.addEventListener('load',function() {
-  const _PokeData = JSON.parse(xhr.response);
-  img1.src=_PokeData.sprites.front_default;
-})
+  xhr.addEventListener('load', function () {
+    const _PokeData = JSON.parse(xhr.response);
+    img1.src = _PokeData.sprites.front_default;
+  })
 
 })
 
 console.dir(randomContainer)
 xhr.open('GET', _URL);
 xhr.send();
-xhr.addEventListener('load', function() {
+xhr.addEventListener('load', function () {
   console.log(xhr.response);
   const _PokeData = JSON.parse(xhr.response);
-  img1.src=_PokeData.sprites.front_default;
+  img1.src = _PokeData.sprites.front_default;
 
-  randomContainer.addEventListener('mouseover', function(){  
-  if(_PokeData.sprites.back_default ==null){
-    img1.src=_PokeData.sprites.front_default
+  randomContainer.addEventListener('mouseover', function () {
+    if (_PokeData.sprites.back_default == null) {
+      img1.src = _PokeData.sprites.front_default
 
-  }else{
-    img1.src=_PokeData.sprites.back_default
-    
-  }
- })
- randomContainer.addEventListener('mouseout', function(){
-    img1.src=_PokeData.sprites.front_default
- 
- })
- 
-//back_default
+    } else {
+      img1.src = _PokeData.sprites.back_default
 
-console.dir(_PokeData)
+    }
+  })
+  randomContainer.addEventListener('mouseout', function () {
+    img1.src = _PokeData.sprites.front_default
+
+  })
+
+  //back_default
+
+  console.dir(_PokeData)
 
 
 });
